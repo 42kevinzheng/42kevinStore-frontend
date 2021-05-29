dcf925f9da538559c2ec603fb2c0a522b400f6a5
Kevin Zheng <42kevinzheng@gmail.com>
2021-05-28 21:08:24 -0400

    navbar image

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import useStyles from './styles';
+import logo from './fun.png';
 
 const Navbar = () => {
 
@@ -12,7 +13,7 @@ const Navbar = () => {
             <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
           <Typography  variant="h6" className={classes.title} color="inherit">
-            <img  alt="" height="25px" className={classes.image} /> Kevin
+            <img src={logo} alt="df" height="100px" className={classes.image} /> Kevin
           </Typography>
           <div className={classes.grow} />
           <div>
